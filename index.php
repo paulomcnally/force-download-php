ea05da1a3303c29ab8897400531f17064d0a8fef
Paulo McNally <paulomcnally@gmail.com>
2013-06-18 19:11:08 -0700

    url from get

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
-$file_url = 'http://www.woo55.pk/adata/3256/06%20-%20Avril%20Lavigne%20-%20My%20Happy%20Ending-(www.SongsLover.com).mp3';
+$file_url = ( isset( $_GET['url'] ) && !empty( $_GET['url'] ) ) ? base64_decode($_GET['url']) : NULL;
+
+if( is_null( $file_url ) ){
+header("Location: https://play.google.com/store/apps/details?id=com.mc.mp3chief");
+exit;
+}
+else{
 $file_name = end( explode( "/", $file_url ) );
-
-
 header('Content-Type: application/octet-stream');
 header("Content-Transfer-Encoding: Binary"); 
 header("Content-disposition: attachment; filename=\"".$file_name."\""); 
 readfile($file_url);
 exit;
+}
+
 ?>
